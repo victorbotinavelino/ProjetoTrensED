5135b30320f8a4aa7b07da8c361737c06b33cb95
KaiFlame <42344215+KaiFlame@users.noreply.github.com>
2019-12-02 16:38:26 -0300

    Getters e Setters de caminho traduzidos

--- Cidade.java
+++ Cidade.java
@@ -1,25 +1,98 @@
-public class Cidade
-{
-    private int indiceColuna, indiceLinha;
-    private String nomeCidade;
-    private int coordenadaX, coordenadaY;
+/// 
+/// Guilherme Salim de Barros - 18188 \\ Victor Botin Avelino - 18172
+///
 
-    public Cidade(nomeNovaCidade)
+    class Cidade : IComparable<Cidade>
     {
-        this.nomeCidade =  nomeNovaCidade;
-    }
+        //Atributos:
+        private int idCidade;
+        private String nome;
+        private int coordenadaX;
+        private int coordenadaY;
 
-    public Cidade(int indiceDaColuna, int indiceDaLinha, String nomeNovaCidade, int coordenadaDeX, int coordenadaDeY)
-    {
-        this.indiceColuna = indiceDaColuna;
-        this.indiceLinha = indiceDaLinha;
-        this.nomeCidade = nomeNovaCidade;
-        this.coordenadaX = coordenadaDeX;
-        this.coordenadaY = coordenadaDeY;
-    }
+        //Constantes dos atributos:
+        const int inicioId = 0;
+        const int tamanhoId = 3;
+        const int inicioNome = inicioId + tamanhoId;
+        const int tamanhoNome = 15;
+        const int inicioX = inicioNome + tamanhoNome;
+        const int tamanhoX = 5;
+        const int inicioY = inicioX + tamanhoX;
+        const int tamanhoY = 5;
 
-    public ToString()
-    {
+        //Getters dos atributos:
+        public int getIdCidade()
+        {
+             return idCidade;
+        }
+        public String getNome() 
+        {
+             return nome;
+        }
+        public int getCoordenadaX()
+        {
+             return coordenadaX;
+        }
+        public int getCoordenadaY()
+        {
+             return coordenadaY;
+        }
 
+        // Setters dos atributos:
+         public int setIdCidade(int novoId) throws Exception
+        {
+             if(novoId == null)
+                    throw new Exception("Parâmetro nulo!");
+
+            this.idCidade = novoId;
+
+        }
+
+        public string setNome(String novoNome) 
+        { 
+             if(novoNome == null)
+                    throw new Exception("Parâmetro nulo!");
+
+            this.nome = novoNome;
+        }
+
+        public int setCoordenadaX(int novaCoordenadaX)
+        { 
+             if(novaCoordenadaX == null)
+                    throw new Exception("Parâmetro nulo!");
+
+            this.coordenadaX = novaCoordenadaX;
+        }
+
+        public int setCoordenadaY(int novaCoordenadaY)
+        { 
+             if(novaCoordenadaY == null)
+                    throw new Exception("Parâmetro nulo!");
+
+            this.coordenadaY = novaCoordenadaY;
+        }
+
+
+        //Construtor da classe. Especificamente, este construtor atribui
+        //aos atributos os dados de uma linha do arquivo texto.
+        public cidade(string linhaArquivo)
+        {
+            this.idCidade = int.Parse(linhaArquivo.Substring(inicioId, tamanhoId));
+            this.nome = linhaArquivo.Substring(inicioNome, tamanhoNome);
+            this.coordenadaX = int.Parse(linhaArquivo.Substring(inicioX, tamanhoX));
+            this.coordenadaY = int.Parse(linhaArquivo.Substring(inicioY, tamanhoY));
+        }
+
+        public cidade(int i)
+        {
+            this.idCidade = i;
+            this.nome = null;
+            this.coordenadaX = 0;
+            this.coordenadaY = 0;
+        }
+
+        public int compareTo(Cidade c)
+        {
+            return this.IdCidade - c.IdCidade;
+        }
     }
-}
